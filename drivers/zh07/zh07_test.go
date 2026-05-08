37a72d0f3c6ed0e8bd70de033e5218223274e35d
Patricio Diaz <padiazg@gmail.com>
2026-05-08 20:12:17 -0300

    added tests for zh07.New

---
+++ drivers/zh07/zh07_test.go
@@ -0,0 +1,56 @@
+package zh07
+
+import (
+	"testing"
+
+	"github.com/go-openapi/testify/v2/assert"
+	"github.com/padiazg/go-aqi/domain"
+)
+
+type NewFn func(*testing.T, domain.SensorProvider)
+
+var checkNew = func(fns ...NewFn) []NewFn { return fns }
+
+func checkType(want domain.SensorProvider) NewFn {
+	return func(t *testing.T, sp domain.SensorProvider) {
+		if want == nil {
+			assert.Empty(t, sp)
+			return
+		}
+		assert.IsType(t, want, sp)
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name   string
+		config *Config
+		checks []NewFn
+	}{
+		{
+			name:   "fail - unknown mode",
+			config: &Config{Mode: -1},
+			checks: checkNew(checkType(nil)),
+		},
+		{
+			name:   "sucess - ModeInitiative",
+			config: &Config{Mode: ModeInitiative},
+			checks: checkNew(checkType(&ZH07i{})),
+		},
+		{
+			name:   "sucess - ModeQA",
+			config: &Config{Mode: ModeQA},
+			checks: checkNew(checkType(&ZH07q{})),
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			r := New(tt.config)
+			for _, c := range tt.checks {
+				c(t, r)
+			}
+		})
+	}
+}
